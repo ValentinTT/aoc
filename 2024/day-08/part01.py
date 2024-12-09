b9cf7d3920eb6b92826b8dc7556984eb475416db
ValentinTT <valentin.tapiatorti@mercadolibre.com>
2024-12-09 00:35:21 -0300

    feat: day 08

---
+++ 2024/day-08/part01.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+from itertools import combinations
+
+
+def read_file():
+    board = defaultdict(list)
+    r, c = 0, 0
+    with open("input-01.txt", "r") as file:
+        for line in file:
+            for x, c in enumerate(list(line[:-1])):
+                if c != ".":
+                    board[c].append((x, r))
+            c = len(line)
+            r += 1
+    return board, r, c
+
+
+def main():
+    nodes, r, c = read_file()
+    new_ant = set()
+
+    def calc_antinode(a, b):
+        antis = set()
+        (ax, ay) = a
+        (bx, by) = b
+        dx = bx - ax
+        dy = by - ay
+
+        cx, cy = ax - dx, ay - dy
+        if is_in_bounds(cx, cy):
+            antis.add((cx, cy))
+        cx, cy = bx + dx, by + dy
+        if is_in_bounds(cx, cy):
+            antis.add((cx, cy))
+
+        return antis
+
+    def is_in_bounds(x, y):
+        return 0 <= x < c and 0 <= y < r
+
+    for freq in nodes:
+        nodes_freq = nodes[freq]
+        for a, b in combinations(nodes_freq, r=2):
+            new_ant |= calc_antinode(a, b)
+    return len(new_ant)
+
+
+if __name__ == "__main__":
+    print(f"{main()}")  # 381
